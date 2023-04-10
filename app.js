8ed4f40e6fdfaccab8e12095824eb84d14782186
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-04-11 01:43:27 +0200

    :rocket: feat: initialize server express + connect to DB + creating models product

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Product = require("./models/Product");
+
+const urlDatabase = process.env.URL_DATABASE;
+
+mongoose
+  .connect(urlDatabase, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
