2b54822aa715a35e913ce0c3ae0bdcec18b94117
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-04-11 01:51:59 +0200

    :rocket: feat: Route get all products + get one product

--- app.js
+++ app.js
@@ -27,4 +27,16 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/api/products", (req, res, next) => {
+  Product.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/products/:id", (req, res, next) => {
+  Product.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
 module.exports = app;
