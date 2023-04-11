219ca605856ee65ef69487f35f9fd5f18c6af0c0
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-04-11 02:04:43 +0200

    :rocket: feat: Route post, put + delete product

--- app.js
+++ app.js
@@ -39,4 +39,26 @@ app.get("/api/products/:id", (req, res, next) => {
     .catch((error) => res.status(404).json({ error }));
 });
 
+app.post("/api/products", (req, res, next) => {
+  delete req.body._id;
+  const product = new Product({
+    ...req.body,
+  });
+  product
+    .save()
+    .then((product) => res.status(201).json({ product }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/products/:id", (req, res, next) => {
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Modified!" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Deleted!" }))
+    .catch((error) => res.status(400).json({ error }));
+});
 module.exports = app;
