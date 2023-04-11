589a89df0ae494f311979539cae07c6e3aa01b42
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-04-11 02:11:10 +0200

    :bug: fix: check all routes to match with front-end

--- app.js
+++ app.js
@@ -29,13 +29,13 @@ app.use((req, res, next) => {
 
 app.get("/api/products", (req, res, next) => {
   Product.find()
-    .then((things) => res.status(200).json(things))
+    .then((products) => res.status(200).json({ products }))
     .catch((error) => res.status(400).json({ error }));
 });
 
 app.get("/api/products/:id", (req, res, next) => {
   Product.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
+    .then((product) => res.status(200).json({ product }))
     .catch((error) => res.status(404).json({ error }));
 });
 
@@ -56,7 +56,7 @@ app.put("/api/products/:id", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
-app.delete("/api/stuff/:id", (req, res, next) => {
+app.delete("/api/products/:id", (req, res, next) => {
   Product.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: "Deleted!" }))
     .catch((error) => res.status(400).json({ error }));
